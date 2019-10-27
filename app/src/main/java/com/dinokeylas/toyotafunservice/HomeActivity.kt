987347c6258fe3e0f8a3e55effae6732c43f55bb
Dino Keylas <dinokeylas@gmail.com>
2019-10-27 10:09:57 +0700

    Add interaction fragment from bottomNavigationBar

--- app/src/main/java/com/dinokeylas/toyotafunservice/HomeActivity.kt
+++ app/src/main/java/com/dinokeylas/toyotafunservice/HomeActivity.kt
@@ -2,11 +2,42 @@ package com.dinokeylas.toyotafunservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val menu: Menu = findViewById<BottomNavigationView>(R.id.bottom_navigation).menu
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        //initialize fragment to show first
+        selectedMenu(menu.getItem(0))
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            item: MenuItem -> selectedMenu(item)
+            false
+        }
+
     }
+
+    private fun selectedMenu(menuItem: MenuItem){
+        menuItem.isChecked = true
+        when(menuItem.itemId){
+            R.id.navigation_home -> showFragment(HomeFragment.newInstance())
+            R.id.navigation_notification -> showFragment(NotificationFragment.newInstance())
+            R.id.navigation_chatbot -> showFragment(ChatBotFragment.newInstance())
+            R.id.navigation_profile -> showFragment(ProfileFragment.newInstance())
+        }
+    }
+
+    private fun showFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+    }
+
 }
