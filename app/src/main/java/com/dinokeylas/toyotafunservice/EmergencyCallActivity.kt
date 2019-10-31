621d6260e4a116978c349b5465da1d35ad1464bd
Dino Keylas <dinokeylas@gmail.com>
2019-10-31 10:21:13 +0700

    Finish emergencyCallActivity

--- app/src/main/java/com/dinokeylas/toyotafunservice/EmergencyCallActivity.kt
+++ app/src/main/java/com/dinokeylas/toyotafunservice/EmergencyCallActivity.kt
@@ -1,22 +1,48 @@
 package com.dinokeylas.toyotafunservice
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.dinokeylas.toyotafunservice.util.Constant.Collection
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
+import kotlin.collections.ArrayList
 
 class EmergencyCallActivity : AppCompatActivity() {
 
-    var list: MutableList<EmergencyCall> = ArrayList()
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: EmergencyCallAdapter
+    lateinit var toolbar: Toolbar
+    var list: ArrayList<EmergencyCall> = ArrayList()
+
+    private val MY_PERMISSIONS_REQUEST_READ_CONTACTS = 20
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emergency_call)
+        checkCallPermission()
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        Objects.requireNonNull(supportActionBar)?.setTitle("Daftar Nomor Darurat")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        recyclerView = findViewById(R.id.rv_emergency_call)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        adapter = EmergencyCallAdapter(this, list)
+        recyclerView.adapter = adapter
 
         val firebaseFirestore = FirebaseFirestore.getInstance()
-
         firebaseFirestore.collection(Collection.EMERGENCY_CALL).get()
             .addOnSuccessListener { documents ->
                 if(documents!=null){
@@ -24,13 +50,26 @@ class EmergencyCallActivity : AppCompatActivity() {
                         val emergencyCall: EmergencyCall? = document.toObject(EmergencyCall::class.java)
                         list.add(emergencyCall!!)
                     }
+                    adapter.notifyDataSetChanged()
                 } else {
-                    //show toast message
                     Log.d("CALL-DATA", "fail to catch user data")
                 }
             }
             .addOnFailureListener {
                 Log.d("CALL-DATA", it.toString())
             }
+    }
+
+    private fun checkCallPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            //ask permission of not yet granted
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE),
+                MY_PERMISSIONS_REQUEST_READ_CONTACTS)
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+            startActivity(Intent(this, HomeActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/dinokeylas/toyotafunservice/EmergencyCallAdapter.kt
@@ -0,0 +1,40 @@
+package com.dinokeylas.toyotafunservice
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class EmergencyCallAdapter(private val context: Context, private val list: ArrayList<EmergencyCall>): RecyclerView.Adapter<EmergencyCallAdapter.ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(LayoutInflater.from(context).inflate(R.layout.layout_emergency_call, parent, false))
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.tvPhoneNumber.text = list[position].phoneNumber
+        holder.tvName.text = list[position].name
+        holder.cvEmergencyCall.setOnClickListener(onClickListener(position))
+    }
+
+    class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var cvEmergencyCall : CardView = itemView.findViewById(R.id.cv_emergency_call)
+        var tvPhoneNumber: TextView =itemView.findViewById(R.id.tv_phone_number)
+        var tvName: TextView = itemView.findViewById(R.id.tv_ec_name)
+    }
+
+    private fun onClickListener(position: Int): View.OnClickListener{
+        return View.OnClickListener {
+            val callIntent = Intent(Intent.ACTION_VIEW)
+            callIntent.data = Uri.parse("tel:"+list[position].phoneNumber)
+            context.startActivity(callIntent)
+        }
+    }
+}
